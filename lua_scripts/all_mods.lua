09c473746932bbb3de9cb171dafccd1250d0b425
teksuprc <teksuprc@yahoo.com>
2021-01-03 17:28:51 -0500

    Add names to some mods.

--- lua_scripts/all_mods.lua
+++ lua_scripts/all_mods.lua
@@ -1,36 +1,41 @@
-Game.AddToInventory("Items.PowerfulFabricEnhancer01", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer02", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer03", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer04", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer05", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer06", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer07", 1)
-Game.AddToInventory("Items.PowerfulFabricEnhancer08", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer01", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer02", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer03", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer04", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer05", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer06", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer07", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer09", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer10", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer11", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer12", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer13", 1)
-Game.AddToInventory("Items.SimpleFabricEnhancer14", 1)
-Game.AddToInventory("Items.SimpleWeaponMod01", 1)
-Game.AddToInventory("Items.SimpleWeaponMod02", 1)
-Game.AddToInventory("Items.SimpleWeaponMod03", 1)
-Game.AddToInventory("Items.SimpleWeaponMod04", 1)
-Game.AddToInventory("Items.SimpleWeaponMod08", 1)
-Game.AddToInventory("Items.SimpleWeaponMod09", 1)
-Game.AddToInventory("Items.SimpleWeaponMod10", 1)
-Game.AddToInventory("Items.SimpleWeaponMod11", 1)
-Game.AddToInventory("Items.SimpleWeaponMod12", 1)
-Game.AddToInventory("Items.SimpleWeaponMod13", 1)
-Game.AddToInventory("Items.SimpleWeaponMod16", 1)
-Game.AddToInventory("Items.SimpleWeaponMod17", 1)
+Game.AddToInventory("Items.ArasakaMeleeWeaponMod", 1) -- kunai
+Game.AddToInventory("Items.PowerfulFabricEnhancer01", 1) -- coolit
+Game.AddToInventory("Items.PowerfulFabricEnhancer02", 1) -- antivenom
+Game.AddToInventory("Items.PowerfulFabricEnhancer03", 1) -- panacea
+Game.AddToInventory("Items.PowerfulFabricEnhancer04", 1) -- superinsulator
+Game.AddToInventory("Items.PowerfulFabricEnhancer05", 1) -- soft-sole
+Game.AddToInventory("Items.PowerfulFabricEnhancer06", 1) -- cut-it-out
+Game.AddToInventory("Items.PowerfulFabricEnhancer07", 1) -- predator
+Game.AddToInventory("Items.PowerfulFabricEnhancer08", 1) -- deadeye
+Game.AddToInventory("Items.SimpleFabricEnhancer01", 1) -- armadillo
+Game.AddToInventory("Items.SimpleFabricEnhancer02", 1) -- resist
+Game.AddToInventory("Items.SimpleFabricEnhancer03", 1) -- fortuna
+Game.AddToInventory("Items.SimpleFabricEnhancer04", 1) -- bully
+Game.AddToInventory("Items.SimpleFabricEnhancer05", 1) -- backpacker
+Game.AddToInventory("Items.SimpleFabricEnhancer06", 1) -- footloose
+Game.AddToInventory("Items.SimpleFabricEnhancer07", 1) -- showtime
+Game.AddToInventory("Items.SimpleFabricEnhancer09", 1) -- osmosis
+Game.AddToInventory("Items.SimpleFabricEnhancer10", 1) -- plume
+Game.AddToInventory("Items.SimpleFabricEnhancer11", 1) -- zero drag
+Game.AddToInventory("Items.SimpleFabricEnhancer12", 1) -- tenacity
+Game.AddToInventory("Items.SimpleFabricEnhancer13", 1) -- vanguard
+Game.AddToInventory("Items.SimpleFabricEnhancer14", 1) -- boom breaker
+Game.AddToInventory("Items.SimpleWeaponMod01", 1) -- crunch
+Game.AddToInventory("Items.SimpleWeaponMod02", 1) -- penatrator
+Game.AddToInventory("Items.SimpleWeaponMod03", 1) -- pacifier
+Game.AddToInventory("Items.SimpleWeaponMod04", 1) -- combat amplifier
+Game.AddToInventory("Items.SimpleWeaponMod08", 1) -- bouncy
+Game.AddToInventory("Items.SimpleWeaponMod09", 1) -- charge spike
+Game.AddToInventory("Items.SimpleWeaponMod10", 1) -- battery
+Game.AddToInventory("Items.SimpleWeaponMod11", 1) -- countermass
+Game.AddToInventory("Items.SimpleWeaponMod12", 1) -- pulverize
+Game.AddToInventory("Items.SimpleWeaponMod13", 1) -- weaken
+Game.AddToInventory("Items.SimpleWeaponMod16", 1) -- autoloader
+Game.AddToInventory("Items.SimpleWeaponMod17", 1) -- pax
+Game.AddToInventory("Items.TygerMeleeWeaponMod", 1) -- white knuckle
+Game.AddToInventory("Items.TygerRangedWeaponMod", 1) -- neon arrow
+Game.AddToInventory("Items.ValentinosMeleeWeaponMod", 1) -- cold shoulder
+Game.AddToInventory("Items.ValentinosRangedWeaponMod", 1) -- vendetta
 Game.AddToInventory("Items.w_att_scope_long_01", 1)
 Game.AddToInventory("Items.w_att_scope_long_02", 1)
 Game.AddToInventory("Items.w_att_scope_long_03", 1)
@@ -46,3 +51,5 @@ Game.AddToInventory("Items.w_silencer_01", 1)
 Game.AddToInventory("Items.w_silencer_02", 1)
 Game.AddToInventory("Items.w_silencer_03", 1)
 Game.AddToInventory("Items.w_silencer_04", 1)
+Game.AddToInventory("Items.WraithsMeleeWeaponMod", 1) -- scourge
+Game.AddToInventory("Items.WraithsRangedWeaponMod", 1) -- phantom
